5d5f2959f992607c45629e81e03ce6d126ceaf3a
MEIMINJUN <251222845@qq.com>
2017-12-27 15:26:08 +0800

    添加菜单

--- wechat/index1.js
+++ wechat/index1.js
@@ -1,26 +1,12 @@
-const wechat = require('./server/index'),
-       config = require('./config');//引入配置文件
+const wechat = require('./server/index')
+const config = require('./config') //引入配置文件
 const koa = require('koa')
 const router = require('./koaserver/router')
-const bodyParser = require('koa-bodyparser')
-const nunjucks = require('koa-nunjucks-2')
-const path = require('path')
-// 引入 koa-static
-const staticFiles = require('koa-static')
 
+const middleware = require('./koaserver/middleware')
 const app = new koa();
-// 指定 public目录为静态资源目录，用来存放 js css images 等
-app.use(staticFiles(path.resolve(__dirname, "./koaserver/public")))
+middleware(app)
 
-app.use(nunjucks({
-  ext: 'html',
-  path: path.join(__dirname, 'koaserver/views'), // 指定视图目录
-  nunjucksConfig: {
-    trimBlocks: true // 开启转义 防Xss
-  }
-}))
-
-app.use(bodyParser())
 router(app)
 //监听3000端口
 app.listen(3000, () => {
