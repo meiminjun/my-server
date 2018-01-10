257b27802d4e343ebddc77cbc6f99baf659f364b
MEIMINJUN <251222845@qq.com>
2018-01-10 12:06:24 +0800

    解决服务器时区问题

--- wechat/tools/clocking/index.js
+++ wechat/tools/clocking/index.js
@@ -10,24 +10,17 @@
 
 var schedule = require('node-schedule') // 定时任务模块
 var moment = require('moment-timezone');
+var nowTime = new Date()
 // var zhongguo1 = moment.tz("2018-01-10 12:00", "Asia/Shanghai").format('YYYY-MM-DD HH:mm:ss');
-var zhongguo = moment().tz("Asia/Shanghai").format('YYYY-MM-DD HH:mm:ss');
-// var type = {
-//   s: '0-59 * * * * *', // 每秒触发
-//   m: '0 * * * * *', // 每分钟触发
-//   h: '0 0 * * * *', // 每小时触发
-//   d: '0 30 9 * * *', // 每天的早上9点30分0秒触发
-//   m1: '0 30 12 25-31 * *', // 每月的25至31日12点30分0秒触发
-//   o: new Date(2018, 0, 9, 22, 53, 0) // 2018年的1月9日22点49分0秒触发, 注意月份要+1,执行完成后会终止进程
-// }
 
 // 每秒都触发
 function scheduleCronstyle (t, callback) {
   return schedule.scheduleJob(t, function () {
     var nowTime = new Date()
-    console.log('scheduleCronstyle:' + nowTime)
-    console.log('zhongguo:' + zhongguo)
-    callback && callback(nowTime)
+    var zhongguo = moment().tz("Asia/Shanghai").format('YYYY-MM-DD HH:mm:ss');
+    var result = '当前时间:' + zhongguo
+    console.log('服务器时间:' + nowTime)
+    callback && callback(result)
   })
 }
 
--- wechat/tools/index.js
+++ wechat/tools/index.js
@@ -6,7 +6,7 @@ var type = {
   s: '0-59 * * * * *', // 每秒触发
   m: '0 * * * * *', // 每分钟触发
   h: '0 0 * * * *', // 每小时触发
-  d: '0 30 14 * * *', // 每天的早上9点30分0秒触发
+  d: '0 10 12 * * *', // 每天的早上9点30分0秒触发
   m1: '0 30 12 25-31 * *', // 每月的25至31日12点30分0秒触发
   o: new Date(2018, 0, 9, 22, 53, 0) // 2018年的1月9日22点49分0秒触发, 注意月份要+1,执行完成后会终止进程
 }
@@ -25,19 +25,37 @@ var sendContent = {
 }
 
 console.log('开始执行!')
-// 每小时邮件触发
+
+// 每秒测试触发
 clocking.scheduleCronstyle({rule: config.type.s, timezone: 'Asia/Shanghai'}, function (nowTime) {
-  sendContent.subject = '每小时触发'
-  console.log('scheduleCronstyle:' + nowTime)
-  // console.log(nowTime);
-  // sendContent.html += `<h1>${nowTime}</h1>`
-  // email.sendMail(sendContent) // 发送邮件
+  console.log('每秒触发:' + nowTime)
+})
+
+// 每小时邮件触发
+clocking.scheduleCronstyle({rule: config.type.h, timezone: 'Asia/Shanghai'}, function (nowTime) {
+  var sendContent = {
+    from: '"管理员-梅敏君" <13265678360@163.com>', // 发件箱地址
+    to: '251222845@qq.com', // 收件箱地址
+    subject: '每小时触发', // 主题
+    // 发送text或者html格式
+    // text: 'Hello world?', // plain text body
+    html: '<h1>Hello world?</h1>' // html body
+  }
+  sendContent.html += `<h1>${nowTime}</h1>`
+  email.sendMail(sendContent) // 发送邮件
   console.log('每小时邮件触发成功！')
 })
 
 // 每天邮件触发
 clocking.scheduleCronstyle({rule: config.type.d, timezone: 'Asia/Shanghai'}, function (nowTime) {
-  sendContent.subject = '每天的早上9点30分0秒触发'
+  var sendContent = {
+    from: '"管理员-梅敏君" <13265678360@163.com>', // 发件箱地址
+    to: '251222845@qq.com', // 收件箱地址
+    subject: '每天的早上9点30分0秒触发', // 主题
+    // 发送text或者html格式
+    // text: 'Hello world?', // plain text body
+    html: '<h1>Hello world?</h1>' // html body
+  }
   sendContent.html += `<h1>${nowTime}</h1>`
   email.sendMail(sendContent) // 发送邮件
   console.log('每天邮件触发成功！')
