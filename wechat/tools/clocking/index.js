56f5aca7a2e8957e1eaa16fcbd44c549f112a385
meiminjun <251222845@qq.com>
2018-01-12 00:15:31 +0800

    添加一个时区插件

--- wechat/tools/clocking/index.js
+++ wechat/tools/clocking/index.js
@@ -9,15 +9,14 @@
 // └───────────────────────── second (0 - 59, OPTIONAL)
 
 var schedule = require('node-schedule') // 定时任务模块
-var moment = require('moment-timezone');
-var nowTime = new Date()
+var moment = require('moment-timezone')
 // var zhongguo1 = moment.tz("2018-01-10 12:00", "Asia/Shanghai").format('YYYY-MM-DD HH:mm:ss');
 
 // 每秒都触发
 function scheduleCronstyle (t, callback) {
   return schedule.scheduleJob(t, function () {
     var nowTime = new Date()
-    var zhongguo = moment().tz("Asia/Shanghai").format('YYYY-MM-DD HH:mm:ss');
+    var zhongguo = moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')
     var result = '当前时间:' + zhongguo
     console.log('服务器时间:' + nowTime)
     callback && callback(result)
