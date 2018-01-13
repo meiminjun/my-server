221573e92ef6c8a13e44fb1137290a4faa35df01
meiminjun <251222845@qq.com>
2018-01-13 15:30:06 +0800

    更新

--- wechat/crawler/containers/sendEmail/index.js
+++ wechat/crawler/containers/sendEmail/index.js
@@ -9,7 +9,7 @@ var type = {
   s: '0-59 * * * * *', // 每秒触发
   m: '0 * * * * *', // 每分钟触发
   h: '0 0 * * * *', // 每小时触发
-  d: '0 10 12 * * *', // 每天的早上9点30分0秒触发
+  d: '0 32 15 * * *', // 每天的早上9点30分0秒触发
   m1: '0 30 12 25-31 * *', // 每月的25至31日12点30分0秒触发
   o: new Date(2018, 0, 9, 22, 53, 0) // 2018年的1月9日22点49分0秒触发, 注意月份要+1,执行完成后会终止进程
 }
@@ -44,20 +44,20 @@ function showTime () {
 //   console.log(nowTime)
 // })
 
-// sendEmail every hour
-agenda.define('sendEmail every hours', function (job, done) {
+// sendEmail every 5 分钟
+agenda.define('sendEmail every mins', function (job, done) {
   var nowTime = showTime()
   var sendContent = {
     from: '"管理员-梅敏君" <13265678360@163.com>', // 发件箱地址
     to: '251222845@qq.com', // 收件箱地址
-    subject: '每小时触发', // 主题
+    subject: job.subject, // 主题
     // 发送text或者html格式
     // text: 'Hello world?', // plain text body
     html: '<h1>Hello world?</h1>' // html body
   }
   sendContent.html += `<h1>${nowTime}</h1>`
   email.sendMail(sendContent, done) // 发送邮件
-  console.log('每小时邮件触发成功！')
+  console.log('每分钟触发成功！')
 })
 
 // 每天发送邮件
@@ -66,7 +66,7 @@ agenda.define('sendEmail every day', function (job, done) {
   var sendContent = {
     from: '"管理员-梅敏君" <13265678360@163.com>', // 发件箱地址
     to: '251222845@qq.com', // 收件箱地址
-    subject: '每天的早上9点30分0秒触发', // 主题
+    subject: job.subject, // 主题
     // 发送text或者html格式
     // text: 'Hello world?', // plain text body
     html: '<h1>Hello world?</h1>' // html body
@@ -77,12 +77,8 @@ agenda.define('sendEmail every day', function (job, done) {
 })
 
 agenda.on('ready', function () {
-  var everyHours = agenda.create('sendEmail every hours', {to: 'another-guy@example.com'})
-  var everyDay = agenda.create('sendEmail every day', {to: 'another-guy@example.com'})
-  everyDay.repeatAt('10:30', {timezone: 'Asia/Shanghai'}).save()
-  everyHours.repeatEvery(type.h, {
-    timezone: 'Asia/Shanghai'
-  }).save()
+  agenda.every(type.m, 'sendEmail every mins', {subject: '每秒触发'}, {timezone: 'Asia/Shanghai'})
+  agenda.every(type.d, 'sendEmail every day', {subject: '每天定点邮件'}, {timezone: 'Asia/Shanghai'})
   agenda.purge((err, numRemoved) => {
     console.log('旧任务被移除: ', numRemoved)
   })
