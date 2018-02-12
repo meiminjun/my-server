6ca2e62959888e8e27a8a42ff81835adb1da46e4
MEIMINJUN <251222845@qq.com>
2018-02-12 15:46:57 +0800

    更新

--- src/containers/wechat/index.js
+++ src/containers/wechat/index.js
@@ -257,9 +257,9 @@ WeChat.prototype.handleMsg = function (ctx, next) {
           }
         }
         console.log('调试1')
-        console.log(ctx.req.query)
+        console.log(query)
         // 判断消息加解密方式，如果未加密则使用明文，对明文消息进行加密
-        reportMsg = ctx.req.query.encrypt_type == 'aes' ? cryptoGraphy.encryptMsg(reportMsg) : reportMsg
+        reportMsg = query.encrypt_type == 'aes' ? cryptoGraphy.encryptMsg(reportMsg) : reportMsg
         ctx.response.body = reportMsg
         // 返回给微信服务器
         // res.send(reportMsg)
